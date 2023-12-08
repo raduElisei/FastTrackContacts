1922031070d743e78592b2071e4ab5ecfc269201
Radu <45122923+raduElisei@users.noreply.github.com>
2023-12-08 17:52:02 +0200

    Add PG DB And Implement Secrets

---
+++ FastTrackContacts/Migrations/20231208154524_ModifyContactListEntry.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastTrackContacts.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyContactListEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntries",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntries",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "ContactListEntries",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntries",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntries",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "ContactListEntries",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
--- FastTrackContacts/Models/ContactListEntry.cs
+++ FastTrackContacts/Models/ContactListEntry.cs
@@ -4,11 +4,21 @@ namespace FastTrackContacts.Models;
 
 public class ContactListEntry
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
+    [Required]
     public ContactType ContactType { get; set; }
-    public required string Name { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
-    public required string Address { get; set; }
-    public required string PhoneNumber { get; set; }
-    public required string Email { get; set; }
+    [Required]
+    [MaxLength(200)]
+    public string Address { get; set; } = string.Empty;
+    [Required]
+    public string PhoneNumber { get; set; } = string.Empty;
+    [Required]
+    [MaxLength(100)]
+    public string Email { get; set; } = string.Empty;
 }
